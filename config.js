4c44ad5f5b8066c8970e937b6968d5c3e466f6ee
toanguyen2002 <85280625+toanguyen2002@users.noreply.github.com>
2021-06-03 15:06:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'IB anh nhé công chúa',
     btnAccept: 'Yêu em <3',
-    messLink: 'https://www.facebook.com/toanguyen020102' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/toanguyen020102' 
 }
